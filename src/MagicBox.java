11de73c100eea96443c821b943bb6634a2470312
nicholaskimsd <league@wts5.attlocal.net>
2018-10-08 18:31:15 -0700

    magic box finish

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
@@ -22,7 +23,14 @@ public class MagicBox extends JPanel implements Runnable, MouseListener {
 	
 	/*
 	 * Work together as a TEAM of 2 or 3 to make this project. We are going to hide secrets within the magic box. 
-	 * When the user clicks on a secret place, stuff will happen.
+	 * When the user .
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * ...
+	 * 0clicks on a secret place, stuff will happen.
 	 * 
 	 * 1. Make the frame respond to mouse clicks.
 	 * 
@@ -79,12 +87,62 @@ public class MagicBox extends JPanel implements Runnable, MouseListener {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-	med.speak("stop");
+		// TODO Auto-generated m
+	if (e.getX()>43&&e.getX()<102&&e.getY()>74&&e.getY()<118) {
+	med.speak("marc is smart");	
+	}
 	
-	med.speak("get some help");
-    //med.speak("the cow goes meow, the dog goes tweet, and the bird goes mooooooooo");
+	if (e.getX()>180 && e.getX()<215 && e.getY()>518 && e.getY()<574) {
+		System.out.println("yeet");
 		
+	
+	
+	
+	JFrame window = new JFrame("bird");
+	JPanel panel = new JPanel();
+	JLabel lable = med.loadImageFromWithinProject("bird-image.png");
+	panel.add(lable);
+	window.add(panel);
+	window.setVisible(true);
+	lable.setVisible(true);
+	panel.setVisible(true);
+	window.pack();
+	}
+	
+	if (e.getX()>166 && e.getX()<198 && e.getY()>834 && e.getY()<898) {
+
+		JFrame wind = new JFrame("dude");
+		JPanel pan = new JPanel();
+		JLabel lab = med.loadImageFromWithinProject("dude.jpeg");
+		pan.add(lab);
+		wind.add(pan);
+		wind.setVisible(true);
+		lab.setVisible(true);
+		pan.setVisible(true);
+		wind.pack();
+	}
+	
+	
+	if (e.getX()>277 && e.getX()<386 && e.getY()>142 && e.getY()<212) {
+
+		JFrame windo = new JFrame("the totem");
+		JPanel pano = new JPanel();
+		JLabel labo = med.loadImageFromWithinProject("tot.jpeg");
+		pano.add(labo);
+		windo.add(pano);
+		windo.setVisible(true);
+		labo.setVisible(true);
+		pano.setVisible(true);
+		windo.pack();
+	}
+	
+	
+	//med.speak("uof");
+    //med.speak"the cow goes meow, the dog goes tweet, and the bird goes mooooooooo");
+	System.out.println(e.getX());
+	System.out.println(e.getY());
+	System.out.println(backgroundImage.getRGB(e.getX(), e.getY()));
+	
 	}
 
 	@Override
@@ -93,26 +151,26 @@ public class MagicBox extends JPanel implements Runnable, MouseListener {
 	//	if (backgroundImage.getRGB(50>e.getY() &&  ) {
 			
 		//}
-		med.speak("why you bully me ");
+		//med.speak("uof ");
 		//backgroundImage.getRGB(keyEvent.getX(), keyEvent.getY()) 
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		med.speak("leave");
+		//med.speak("uof");
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		med.speak("go away");
+		//med.speak("oof");
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		med.speak("thank you, kind sin");
+		//med.speak("oof");
 	}
 
 }
